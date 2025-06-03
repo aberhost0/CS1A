979556daf4ee48972ed15b642498095670eed9cd
Alyssa Berhost <aberhost0@saddleback.edu>
2025-06-03 00:10:35 +0000

    Created Homework 2F

---
+++ chapterOne/homework2.cpp
@@ -0,0 +1,22 @@
+// Attached: HW_2a, 2b, 2c, 2d, 2e, 2f
+// ======================================================================
+// Program: HW_2f
+// ======================================================================
+// Programmer: Alyssa Berhost
+// Class: CS 1A
+// ======================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  birthday cake
+  return 0;
+}
+// ======================================================================
+/* OUTPUT:
+Enter your first initial: A
+Enter your last initial: B
+Your first initial is A and your last initial is B.
+@aberhost0 ➜ /workspaces/Practice (main) $   */
+// ======================================================================
