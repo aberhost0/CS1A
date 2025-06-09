e82fd29775ba0df5301a64b11f1ffc3f9b5e87aa
Alyssa Berhost <aberhost0@saddleback.edu>
2025-06-09 21:56:09 +0000

    Homework 3

--- homework_3/remainder.cpp
+++ homework_3/remainder.cpp
@@ -10,13 +10,23 @@
 using namespace std;
 
 int main() { 
+   int firstNum;
+   int secondNum;
 
-    
+   cout << "Enter the first number: ";
+   cin >> firstNum;
+   cout << "Enter the second number: ";
+   cin >> secondNum;
 
+   cout << firstNum << " divided by " << secondNum << " equals " << firstNum / secondNum
+      << " with a remainder of " << firstNum % secondNum << "." << endl;
 
 return 0;
 }
 // ======================================================================
 /* OUTPUT:
-   */
+Enter the first number: 126
+Enter the second number: 31
+126 divided by 31 equals 4 with a remainder of 2.
+@aberhost0 ➜ /workspaces/CS1A/homework_3 (main) $ */
 // ======================================================================
