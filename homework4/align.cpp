094e4e5ba734aade8ab208b394985c9d7eb3c15c
Alyssa Berhost <aberhost0@saddleback.edu>
2025-06-17 02:56:27 +0000

    homework 4

---
+++ homework4/align.cpp
@@ -0,0 +1,32 @@
+// Attached: HW_4a
+// ======================================================================
+// Program: Aligning Integers
+// ======================================================================
+// Programmer: Alyssa Berhost
+// Class: CS 1A
+// ======================================================================
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() { 
+   int num1 = 0;
+   int num2 = 0;
+   int num3 = 0;
+   int num4 = 0;
+   int num5 = 0;
+
+   cout << "Enter 5 integers: \n";
+   cin >> num1 >> num2 >> num3 >> num4 >> num5;
+
+   cout << "The five numbers entered are: \n" << 
+
+
+
+    return 0;
+}
+// ======================================================================
+/* OUTPUT:
+   */
+// ======================================================================
