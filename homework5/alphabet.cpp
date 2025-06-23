aade91f5926605a368ec5fc199c8f5df364f35f6
Alyssa Berhost <aberhost0@saddleback.edu>
2025-06-23 01:48:58 +0000

    alphabet determination

---
+++ homework5/alphabet.cpp
@@ -0,0 +1,30 @@
+// Attached: HW_5a
+// ======================================================================
+// Program: Alphabet
+// ======================================================================
+// Programmer: Alyssa Berhost
+// Class: CS 1A
+// ======================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() { 
+   char key;
+
+   cout << "Enter a keyboard character and press Enter: \n";
+   cin >> key;
+
+   if() {
+      cout << "The character is an alphabetic character.\n";
+   }
+   else {
+      cout << "The character is not an alphabetic character. \n";
+   }
+
+return 0;
+}
+// ======================================================================
+/* OUTPUT:
+   */
+// ======================================================================
