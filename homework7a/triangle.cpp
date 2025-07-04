061d73adefd8df8e61a803e01d932b401286df58
Alyssa Berhost <aberhost0@saddleback.edu>
2025-07-04 06:27:22 +0000

    determining right triangle

---
+++ homework7a/triangle.cpp
@@ -0,0 +1,61 @@
+// Attached: HW_7a1
+// =============================================================================
+// Program: Right Triangle
+// =============================================================================
+// Programmer: Alyssa Berhost
+// Class: CS 1A
+// =============================================================================
+
+#include <iostream>
+using namespace std;
+
+float getSide();
+float calcSideC(float sideA, float sideB, float sideC);
+
+// === main ====================================================================
+int main() { 
+    float sideA;
+    float sideB;
+
+    cout << "Enter two sides of a right triangle:\n\n"
+        << "Side A: \n";
+    getSide();
+    sideA = getSide(side);
+    
+    cout << "Side B:\n";
+    sideB = getSide(side);
+
+
+    return 0;
+}               // end of main
+// =============================================================================
+
+
+
+// === getSide =================================================================
+float getSide() {
+    float side;
+
+    cout << "Please enter the dimension: ";
+    cin >> side;
+
+    return side;
+}  // end of getSide
+// =============================================================================
+
+
+
+// === calcSideC ================================================================
+float calcSideC(float sideA, float sideB, float sideC) {
+
+
+    return sideC;
+}
+// =============================================================================
+
+
+
+// =============================================================================
+/* OUTPUT:
+   */
+// =============================================================================
