0c46b70a170b8a017d927e6ca1661b72fd1c3a90
Alyssa Berhost <aberhost0@saddleback.edu>
2025-07-06 17:04:32 +0000

    writting square roots to a new file

--- lab8/files.cpp
+++ lab8/files.cpp
@@ -23,47 +23,54 @@ int main() {
 
     do {
         // User enters desired file to access 
-        // cout << "What file do you want to read? (a, b, c.txt)? ";
-        // cin >> file;
-        // cout << endl;
+        cout << "What file do you want to read? (a, b, c.txt)? ";
+        cin >> file;
+        cout << endl;
 
-        infile.open("a.txt");
+        infile.open(file);
 
         // Check the fail state
         if(outfile.fail()) {
             cout << "Error! File did not open.\n"
-                << "Now closing program...\n";
+                 << "Now closing program...\n";
             exit(EXIT_FAILURE);
         }
-        else {
-            cout << "The file has been read.\n\n";
-        }
+        
+        cout << "The file has been read.\n\n";
 
+        // Open file for square roots
+        outfile.open("squareRoots.txt");
+
+        // Check the fail state
+        if(outfile.fail()) {
+            cout << "Error! File did not open.\n"
+                 << "Now closing program...\n";
+            exit(EXIT_FAILURE);
+        }
+        
         // Read the file entered by the user
         while (!infile.eof()) {
 
-            infile >> num;
-
             // Calculate the square root
+            infile >> num;
             sqrRoot = sqrt(num);
 
-            // Output square roots into a new file
-            outfile.open("squareRoots.txt");
-
-            // Check the fail state
-            if(outfile.fail()) {
-                cout << "Error! File did not open.\n"
-                        << "Now closing program...\n";
-                exit(EXIT_FAILURE);
-            }
-            
+            // Output square root to new file
             outfile << fixed << setprecision(2) << sqrRoot << endl;
-
-            outfile.close();
         }
 
-        cout << "Run the program again (Y/N)? ";
+        cout << "The square roots have been written to the file squareRoots.txt\n";
+
+        infile.close();
+        outfile.close();
+
+        cout << "\nRun the program again (Y/N)? ";
         cin >> choice;
+
+        if (toupper(choice) == 'Y') {
+            
+            system("clear");
+        }
 
     } while (toupper(choice) == 'Y');
 
@@ -75,5 +82,30 @@ int main() {
 
 // ==========================================================================
 /* OUTPUT:
-*/
+What file do you want to read? (a, b, c.txt)? a.txt
+
+The file has been read.
+
+The square roots have been written to the file squareRoots.txt
+
+Run the program again (Y/N)? y
+
+---- screen clears ---- 
+What file do you want to read? (a, b, c.txt)? b.txt
+
+The file has been read.
+
+The square roots have been written to the file squareRoots.txt
+
+Run the program again (Y/N)? y
+
+---- screen clears ----
+What file do you want to read? (a, b, c.txt)? c.txt
+
+The file has been read.
+
+The square roots have been written to the file squareRoots.txt
+
+Run the program again (Y/N)? n
+@aberhost0 ➜ /workspaces/CS1A/lab8 (main) $ */
 // ==========================================================================
