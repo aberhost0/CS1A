e870ead1342f4773f9974d478fa56c6fae4c97aa
Alyssa Berhost <aberhost0@saddleback.edu>
2025-07-24 02:46:32 +0000

    beginning classes

---
+++ homework10/Student.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student 
+{
+    private: 
+        int idNum;
+        string studentName;
+
+    public: 
+        Student();
+        Student(int idNum, string studentName);
+        ~Student();
+        void setID(int idNum, string studentName);
+        void setName(string studentName);
+        int getID();
+        string getName();
+        void displayStudent();
+}; 
